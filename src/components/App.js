bb906b37f4f69417edd8f781022353e515a5e8b2
varnika1221 <136799326+varnika1221@users.noreply.github.com>
2024-11-08 19:01:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import Navbar from "../components/Navbar"
 import Search from "../components/Search"
 import Products from "../components/Products"
-import Aboutus from "..components/Aboutus"
+import Aboutus from './components/Aboutus';
+
 import Footer from "..components/Footer"
 
 function App(){
